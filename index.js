449dce0f391b074b07f8a7798662623ce3878638
DistantDig <jameswaller711@gmail.com>
2023-10-31 19:20:40 -0700

    added dependant questions

--- index.js
+++ index.js
@@ -3,16 +3,82 @@ const inquirer = require('inquirer');
 
 // Prompt Choices
 const questions = [
+    // Inital question
     {
         type: 'list',
         message: 'What would you like to do?',
         name: 'first',
         choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+    },
+    // Questions for adding a department
+    {
+        type: 'input',
+        message: 'What is the department name?',
+        name: 'addDepartmentName',
+        when: (questions) => questions.first == 'add a department'
+    },
+    // Questions for adding a role
+    {
+        type: 'input',
+        message: 'What is the role title?',
+        name: 'addRoleTitle',
+        when: (questions) => questions.first == 'add a role'
+    },
+    {
+        type: 'input',
+        message: 'What is the role salary?',
+        name: 'addRoleSalary',
+        when: (questions) => questions.first == 'add a role'
+    },
+    {
+        type: 'input',
+        message: 'What is the department id of the role?',
+        name: 'addRoleDepartment',
+        when: (questions) => questions.first == 'add a role'
+    },
+    // Questions for adding a employee
+    {
+        type: 'input',
+        message: 'What is the first name of the employee?',
+        name: 'addEmployeeFirst',
+        when: (questions) => questions.first == 'add an employee'
+    },
+    {
+        type: 'input',
+        message: 'What is the last name of the employee?',
+        name: 'addEmployeeLast',
+        when: (questions) => questions.first == 'add an employee'
+    },
+    {
+        type: 'input',
+        message: 'What is the role id of the employee?',
+        name: 'addEmployeeRole',
+        when: (questions) => questions.first == 'add an employee'
+    },
+    {
+        type: 'input',
+        message: 'What is the manager id of the employee?',
+        name: 'addEmployeeManager',
+        when: (questions) => questions.first == 'add an employee'
+    },
+    // Questions for updating a department
+    {
+        type: 'input',
+        message: 'What is the id of the employee you would like to update?',
+        name: 'updateEmployeeId',
+        when: (questions) => questions.first == 'update an employee role'
+    },
+    {
+        type: 'input',
+        message: 'What is the new role id of the employee?',
+        name: 'updateEmployeeRole',
+        when: (questions) => questions.first == 'update an employee role'
     }
 ];
 
+// Function to call sql queries
 function executeChoice(choice) {
-    switch(choice) {
+    switch(choice.first) {
         case 'view all departments':
           // code block
           break;
